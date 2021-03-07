94e0893f08f2a594bd5bde12438206470bca2a39
dev <dev@localhost.localdomain>
2021-03-07 15:26:28 +0300

    Adding reducer to counter example

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -2,6 +2,8 @@ import React, {useState} from 'react';
 
 import {View,Button, FlatList} from 'react-native'
 
+
+
 const ColorScreen = (props) => {
 
     const [colors, setColors] = useState([])
--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,21 +1,30 @@
-import React , {useState} from 'react';
+import React , { useReducer} from 'react';
 
 import {View, Text, StyleSheet, Button} from 'react-native'
 
+
+let CHANGE_INC = 1
+
+const reducer = (state, action) => {
+
+    state = {...state, count: state.count + action.change}
+
+    return state
+    
+}
+
 const CounterScreen = (props) => {
-    const [counter, setCounter] = useState(0)
+    const [state, dispatch] = useReducer(reducer, {count : 0})
+    const {count} = state
     return (
         <View>
             <Button
-            title="Increase" onPress={() => {
-                setCounter(counter + 1)
-                }}/>
+            title="Increase" onPress={() => dispatch({change: CHANGE_INC})
+                }/>
             <Button
-            title="Decrease"  onPress={() => {
-                setCounter(counter - 1)
-                }}/>
+            title="Decrease"  onPress={() => dispatch({ change: -CHANGE_INC}) }/>
 
-            <Text>Current count - {counter}</Text>
+            <Text>Current count - {count}</Text>
             
         </View>
     );
